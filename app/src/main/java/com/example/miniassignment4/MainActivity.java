2d8ada6c88f3e02df7b3ac6e4588260f531d489b
unknown <mr.saharat.u@gmal.com>
2026-08-28 14:41:24 +0700

    openElse

--- app/src/main/java/com/example/miniassignment4/MainActivity.java
+++ app/src/main/java/com/example/miniassignment4/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.miniassignment4;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        final TextView outputText = findViewById(R.id.tvAreaResult);
+        final EditText radiusInput = findViewById(R.id.etRadius);
+        radiusInput.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(keyEvent.getAction()==KeyEvent.ACTION_DOWN){
+                    if(i==KeyEvent.KEYCODE_ENTER){
+                        if(radiusInput.getText().toString().isEmpty()){
+                            outputText.setText(getText(R.string.empty_input_warning));
+                        }else{
+                                    double radius = Double.parseDouble(radiusInput.getText().toString());
+                                    
+                        }
+
+                    }
+                }
+                return false;
+            }
         });
+
     }
 }
