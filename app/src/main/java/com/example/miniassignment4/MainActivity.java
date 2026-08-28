056fe994e50a233bb5f28c53c6670beb6782af59
unknown <mr.saharat.u@gmal.com>
2026-08-28 14:47:40 +0700

    finish on_key

--- app/src/main/java/com/example/miniassignment4/MainActivity.java
+++ app/src/main/java/com/example/miniassignment4/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.miniassignment4;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,22 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final TextView outputText = findViewById(R.id.tvAreaResult);
         final EditText radiusInput = findViewById(R.id.etRadius);
-        radiusInput.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                if(keyEvent.getAction()==KeyEvent.ACTION_DOWN){
-                    if(i==KeyEvent.KEYCODE_ENTER){
-                        if(radiusInput.getText().toString().isEmpty()){
-                            outputText.setText(getText(R.string.empty_input_warning));
-                        }else{
-                                    double radius = Double.parseDouble(radiusInput.getText().toString());
-                                    
-                        }
-
+        radiusInput.setOnKeyListener((view, i, keyEvent) -> {
+            if(keyEvent.getAction()==KeyEvent.ACTION_DOWN){
+                if(i==KeyEvent.KEYCODE_ENTER){
+                    if(radiusInput.getText().toString().isEmpty()){
+                        outputText.setText(getText(R.string.empty_input_warning));
+                    }else{
+                                double radius = Double.parseDouble(radiusInput.getText().toString());
+                                double area = Math.PI * radius * radius;
+                                 DecimalFormat df = new DecimalFormat( "0.0000");
+                                 outputText.setText(df.format(area));
                     }
                 }
-                return false;
             }
+            return false;
         });
 
     }
